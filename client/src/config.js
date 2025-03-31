150ad70e82e27c722792232a741aed129098ac1a
shivamgit0302 <shivamtiwari100@gmail.com>
2025-04-01 04:06:37 +0530

    Updated config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 // Base URL for API requests
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://your-vercel-deployment-url.vercel.app/api"
+    ? "https://smart-product-assistant-delta.vercel.app/api"
     : "http://localhost:5000/api";
 
 const config = { API_URL };
