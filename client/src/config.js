384756f2f2ef70f59c8da876fca8cb2ad36bc13b
shivamgit0302 <shivamtiwari100@gmail.com>
2025-04-01 05:27:26 +0530

    backend url updated in client

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,8 @@
 // Base URL for API requests
 const API_URL =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5000/api";
+  process.env.NODE_ENV === "production"
+    ? "https://backend-smart-product-assistant.vercel.app/api"
+    : "http://localhost:5000/api";
 
 const config = { API_URL };
 
