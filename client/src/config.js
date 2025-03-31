b6779f3ade0f56cca792e2a9c4a56a17f15f8a59
shivamgit0302 <shivamtiwari100@gmail.com>
2025-04-01 04:27:51 +0530

    updated config.js got which will use rel;ative path for vercel routing

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,6 @@
 // Base URL for API requests
 const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://smart-product-assistant-delta.vercel.app/api"
-    : "http://localhost:5000/api";
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5000/api";
 
 const config = { API_URL };
 
